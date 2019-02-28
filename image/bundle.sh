7c621bda9b8ee1582594f2766e6bf200f9df3194
Max Meldrum <max@meldrum.se>
2019-02-28 14:55:50 +0100

    Add config and update bundle script

--- image/bundle.sh
+++ image/bundle.sh
@@ -2,4 +2,4 @@
 
 mkdir -p rootfs
 docker build -t enya_base .
-docker export $(docker create enya_base) | tar -C rootfs -xvf - && runc spec
+docker export $(docker create enya_base) | tar -C rootfs -xvf - 
